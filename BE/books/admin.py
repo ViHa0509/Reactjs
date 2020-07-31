c04a40b1f388c3ba28b705eef42a7f08debe5019
ViHa0509 <43596575+ViHa0509@users.noreply.github.com>
2020-08-01 01:04:24 +0700

    update

--- BE/books/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from books.models import Author
-# Register your models here.
-admin.site.register(Author)
--- BE/books/serializers.py
+++
@@ -1,8 +0,0 @@
-from books.models import Author
-from rest_framework import serializers
-
-class AuthorSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Author
-        fields = '__all__'
-
